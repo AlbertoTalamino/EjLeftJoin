b1f369ffc2e8d4dd1a7b9ac5329f64cba5986506
DESKTOP-P58U6P6\Talamino <csi21-atallez@colegioaltair.net>
2023-01-12 10:49:32 +0100

    no message

---
+++ LeftJoin/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftJoin
+{
+    internal class Persona
+    {
+        int IdColegio;
+        string CodColegio;
+
+        public int IdColegio1 { get => IdColegio; set => IdColegio = value; }
+        public string CodColegio1 { get => CodColegio; set => CodColegio = value; }
+    }
+}
--- LeftJoin/Program.cs
+++ LeftJoin/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using LeftJoin;
+
+List<Persona> persona = new List<Persona>();
+List<Colegio> colegio = new List<Colegio>;
+
+from p in persona
+
+join co in colegio
+from pco in PersonasColegio.DefaultIfEmpty()
+select new
+{
+    //lo que quieras seleccionar, por ejemplo codPersona = p.CodPersona,
+}
